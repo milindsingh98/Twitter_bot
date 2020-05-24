269724884e76562d0acc633f927dd1a53ff299a6
Milind Singh Dev <31711593+milindsingh98@users.noreply.github.com>
2020-05-24 09:00:52 +0530

    Uploaded .py file

---
+++ tweety.py
@@ -0,0 +1,43 @@
+import tweepy
+import time
+auth = tweepy.OAuthHandler('pYEzh229mS1TA1UQzA0zp1bKh', 'AfLxNhgfxFuahghwk14gATpo9j5WFUg2VwX0369dwuzx01DNfj')  #inclues consumer key and consumer secret
+auth.set_access_token('795172476200202240-FkRDVj7Ot0jpYTjsYUK6ORbAXM7z7AS', 'LUgiOyLnOdvyKqbVGw4MClAx00teghe0aaM8vcXa1JnDP') #api key and secret
+
+api = tweepy.API(auth)
+
+user = api.me()
+
+'''print(user.name)
+print(user.screen_name)
+print(user.followers_count)'''
+
+#super generous bot follow back
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+
+    except tweepy.RateLimitError:
+        time.sleep(1000)
+
+# for follower in (tweepy.Cursor(api.followers).items()):
+#     if follower.name == 'Bhavy':
+#         follower.unfollow()
+#         break
+#     print(follower.name)
+    
+
+search_string = 'python'
+numberOFtweets = 2
+
+for tweet in tweepy.Cursor(api.search, search_string).items(numberOFtweets):
+    try:
+        tweet.favorite()
+        print('I liked that tweet')
+    except tweepy.TweepError as e:
+        print(e.error)
+    except StopIteration:
+        break
+
+
+
